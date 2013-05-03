12f254f2867ea10474120be55298c6a12601825e
piecioshka <piecioshka@gmail.com>
2013-05-03 15:44:53 +0200

    done tasks: zad6 & zad7

--- source/zad7/server.c
+++ source/zad7/server.c
@@ -1,3 +1,33 @@
-int main() {
+/* server - lab1-2 - zadanie 7 */
+
+#include "../../library/utils.h"
+
+#define PORT 13
+#define MAX_QUEUE 128
+
+int main () {
+    /* test_console_lib(); */
+    int sock_id;
+    struct sockaddr_in address;
+
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = htonl(INADDR_ANY);
+    address.sin_port = htons(PORT);
+    memset( &( address.sin_zero ), '\0', 8 );
+
+    printf("=== create server ===\n\n");
+
+    /* create socket */
+    sock_id = create_socket_tcp();
+
+    /* bind server to defined port */
+    bind_port(sock_id, address);
+
+    /* server waiting for client */
+    listen_for_client(sock_id, MAX_QUEUE);
+
+    /* handle client */
+    handle_incoming_client(sock_id);
+
     return 0;
 }
--- source/zad8/client.c
+++ source/zad8/client.c
@@ -1,3 +1,33 @@
-int main() {
+/* client - lab3-4 - zadanie 8 */
+
+#include "../../library/utils.h"
+
+#define PORT 13
+#define DESC_IP "127.0.0.1"
+
+int main () {
+    /* test_console_lib(); */
+    int sock_id;
+    struct sockaddr_in address;
+
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = inet_addr(DESC_IP);
+    address.sin_port = htons(PORT);
+    memset( &( address.sin_zero ), '\0', 8 );
+
+    printf("=== create client instance ===\n\n");
+
+    /* create socket to connection */
+    sock_id = create_socket_tcp();
+
+    /* use create socket to connect address */
+    create_connection(sock_id, address);
+
+    /* receive data from server */
+    receive_from_server(sock_id);
+
+    /* close connection */
+    close_connection(sock_id);
+
     return 0;
 }
