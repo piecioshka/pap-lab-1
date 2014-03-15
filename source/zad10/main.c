5e6dd855cc4dc92a4720b555f2e992d6443639b5
Piotr Kowalski <piecioshka@gmail.com>
2014-03-16 00:12:29 +0100

    File for task no 10 & 11

---
+++ source/zad10/main.c
@@ -0,0 +1,79 @@
+/**
+ * 10. Zamiana nazwy domenowej na adres IP
+ * Napisz program, który będzie działał następująco:
+ *  - jeśli podasz adres IP, program wyświetli nazwę domenową.
+ *  - jeśli podasz nazwę domenową, program wyświetli numer IP.
+ */
+
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netinet/ip.h>
+#include <netdb.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "../lib/libnetutils.h"
+
+void resolve_domain(char * domain) {
+    printf("resolve domain %s\n", domain);
+
+    struct hostent * he;
+    int i;
+    struct in_addr **addr_list;
+
+    if ((he = gethostbyname(domain)) == NULL) {
+        fprintf(stderr, "Bad domain name\n");
+        exit(1);
+    }
+
+    addr_list = (struct in_addr **)he->h_addr_list;
+    for(i = 0; addr_list[i] != NULL; i++) {
+        printf("IP Address: %s ", inet_ntoa(*addr_list[i]));
+    }
+    printf("\n");
+}
+
+void resolve_address(char * ip) {
+    printf("resolve ip address %s\n", ip);
+
+    struct hostent *he;
+    struct in_addr ipv4addr;
+
+    inet_pton(AF_INET, ip, &ipv4addr);
+    if ((he = gethostbyaddr(&ipv4addr, sizeof ipv4addr, AF_INET)) == NULL) {
+        fprintf(stderr, "Bad IP address name\n");
+        exit(1);
+    }
+    printf("Hostname: %s\n", he->h_name);
+}
+
+int main(int argc, char * argv[]) {
+    /* availability options */
+    char * opts = "d:i:";
+    /* processed option */
+    int c;
+
+    if (argc < 2) {
+        fprintf(stderr, "Zbyt malo parametrów\n");
+        exit(1);
+    } else {
+        while ((c = getopt(argc, argv, opts)) != -1) {
+            switch (c) {
+                case 'd':
+                    resolve_domain(optarg);
+                    break;
+                case 'i':
+                    resolve_address(optarg);
+                    break;
+
+                /* for unrecognized option */
+                case '?':
+                default:
+                    printf("Ignore passed options\n");
+            }
+        }
+    }
+
+    exit(0);
+}
+
---
+++ source/zad11/main.c
@@ -0,0 +1,108 @@
+/**
+ * 11. Zamiana nazwy usługi na numer portu
+ * Napisz program, który będzie działał następująco:
+ *  - jeśli podasz numer usługi, program wyświetli jej nazwę
+ *  - jeśli podasz nazwę usługi, program wyświetli jej numer
+ */
+
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netinet/ip.h>
+#include <netdb.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "../lib/libnetutils.h"
+
+void resolve_name(char * servname, char * proto) {
+    struct servent * ent;
+    printf("resolve name %s / %s\n\n", servname, proto);
+
+    printf("querying for servname=%s, proto=%s\n", servname, proto); 
+
+    /* get the service record */
+    if((ent = getservbyname(servname, proto)) == NULL) {
+        perror("getservbyname returned NULL");
+        exit(1);
+    }
+
+    //   struct servent {
+    //       char    *s_name;        /* official service name */
+    //       char    **s_aliases;    /* alias list */
+    //       int     s_port;         /* port number */
+    //       char    *s_proto;       /* protocol to use */
+    //   }
+
+    printf("name=%s\n", ent->s_name);
+
+    int i;
+    printf("aliases:\n"); 
+    for (i=0; ent->s_aliases[i]; i++) {
+        printf("  %s\n", ent->s_aliases[i]);
+    } 
+    short port = ntohs(ent->s_port); 
+    printf("port=%d proto=%s\n", port, ent->s_proto); 
+}
+
+void resolve_number(char * number_s, char * proto) {
+    int serv_port = atoi(number_s);
+    int number = htons(serv_port);
+    printf("resolve number %d / %s\n\n", serv_port, proto);
+    struct servent * ent;
+
+    printf("querying for number=%d, proto=%s\n", number, proto); 
+
+    /* get the service record */
+    if((ent = getservbyport(number, proto)) == NULL) {
+        perror("getservbyname returned NULL");
+        exit(1);
+    }
+
+    //   struct servent {
+    //       char    *s_name;        /* official service name */
+    //       char    **s_aliases;    /* alias list */
+    //       int     s_port;         /* port number */
+    //       char    *s_proto;       /* protocol to use */
+    //   }
+
+    printf("name=%s\n", ent->s_name);
+
+    int i;
+    printf("aliases:\n"); 
+    for (i=0; ent->s_aliases[i]; i++) {
+        printf("  %s\n", ent->s_aliases[i]);
+    } 
+    short port = ntohs(ent->s_port); 
+    printf("port=%d proto=%s\n", port, ent->s_proto); 
+}
+
+int main(int argc, char * argv[]) {
+    /* availability options */
+    char * opts = "n:N:";
+    /* processed option */
+    int c;
+
+    if (argc < 2) {
+        fprintf(stderr, "Zbyt malo parametrów\n");
+        exit(1);
+    } else {
+        while ((c = getopt(argc, argv, opts)) != -1) {
+            switch (c) {
+                case 'n':
+                    resolve_name(optarg, "tcp");
+                    break;
+                case 'N':
+                    resolve_number(optarg, "tcp");
+                    break;
+
+                /* for unrecognized option */
+                case '?':
+                default:
+                    printf("Ignore passed options\n");
+            }
+        }
+    }
+
+    exit(0);
+}
+
