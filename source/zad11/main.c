96bb80cb42fc13974a3b9eb327d2e6095b443d70
Piotr Kowalski <piecioshka@gmail.com>
2014-03-16 12:53:17 +0100

    Compatibility with ISO C90

--- source/zad11/main.c
+++ source/zad11/main.c
@@ -15,7 +15,10 @@
 #include "../lib/libnetutils.h"
 
 void resolve_name(char * servname, char * proto) {
+    int i;
+    short port;
     struct servent * ent;
+
     printf("resolve name %s / %s\n\n", servname, proto);
 
     printf("querying for servname=%s, proto=%s\n", servname, proto); 
@@ -26,31 +29,26 @@ void resolve_name(char * servname, char * proto) {
         exit(1);
     }
 
-    //   struct servent {
-    //       char    *s_name;        /* official service name */
-    //       char    **s_aliases;    /* alias list */
-    //       int     s_port;         /* port number */
-    //       char    *s_proto;       /* protocol to use */
-    //   }
-
     printf("name=%s\n", ent->s_name);
 
-    int i;
     printf("aliases:\n"); 
-    for (i=0; ent->s_aliases[i]; i++) {
+    for (i = 0; ent->s_aliases[i]; i++) {
         printf("  %s\n", ent->s_aliases[i]);
-    } 
-    short port = ntohs(ent->s_port); 
-    printf("port=%d proto=%s\n", port, ent->s_proto); 
+    }
+    port = ntohs(ent->s_port);
+    printf("port=%d proto=%s\n", port, ent->s_proto);
 }
 
 void resolve_number(char * number_s, char * proto) {
+    int i;
     int serv_port = atoi(number_s);
     int number = htons(serv_port);
+    short port;
+
     printf("resolve number %d / %s\n\n", serv_port, proto);
     struct servent * ent;
 
-    printf("querying for number=%d, proto=%s\n", number, proto); 
+    printf("querying for number=%d, proto=%s\n", number, proto);
 
     /* get the service record */
     if((ent = getservbyport(number, proto)) == NULL) {
@@ -58,22 +56,14 @@ void resolve_number(char * number_s, char * proto) {
         exit(1);
     }
 
-    //   struct servent {
-    //       char    *s_name;        /* official service name */
-    //       char    **s_aliases;    /* alias list */
-    //       int     s_port;         /* port number */
-    //       char    *s_proto;       /* protocol to use */
-    //   }
-
     printf("name=%s\n", ent->s_name);
 
-    int i;
     printf("aliases:\n"); 
-    for (i=0; ent->s_aliases[i]; i++) {
+    for (i = 0; ent->s_aliases[i]; i++) {
         printf("  %s\n", ent->s_aliases[i]);
-    } 
-    short port = ntohs(ent->s_port); 
-    printf("port=%d proto=%s\n", port, ent->s_proto); 
+    }
+    port = ntohs(ent->s_port);
+    printf("port=%d proto=%s\n", port, ent->s_proto);
 }
 
 int main(int argc, char * argv[]) {
